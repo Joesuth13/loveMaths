8257c501eba0a792c4626d0958b42a2e6f8970bb
Samuel Sutherland <joesuth13@gmail.com>
2024-11-06 11:17:23 +0000

    create button alerts and main game rng

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
-function runGame() {
+//DOMfinish loading listener
+document.addEventListener("DOMContentLoaded", function(){
+//get buttons, add listeners
+let buttons = document.getElementsByTagName("button");
+for (let button of buttons){
+    button.addEventListener("click", function(){
+        if (this.getAttribute("data-type") === "submit"){
+            alert("You clicked submit");
+        } else {
+           let gameType =  this.getAttribute("data-type");
+           alert(`You clicked ${gameType}`);
+        }
+    })
+}
+})
 
+
+
+
+function runGame() {
+document.addEventListener
 }
 function checkAnswer() {
 
