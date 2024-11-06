d2f70be3100078e16ce39524ae11daaa01967e9d
Samuel Sutherland <joesuth13@gmail.com>
2024-11-06 11:25:15 +0000

    create display additions logic and set default game type

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,35 @@
 //DOMfinish loading listener
 document.addEventListener("DOMContentLoaded", function(){
 //get buttons, add listeners
-let buttons = document.getElementsByTagName("button");
-for (let button of buttons){
-    button.addEventListener("click", function(){
-        if (this.getAttribute("data-type") === "submit"){
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit"){
             alert("You clicked submit");
-        } else {
-           let gameType =  this.getAttribute("data-type");
-           alert(`You clicked ${gameType}`);
-        }
-    })
-}
+            } else {
+            let gameType =  this.getAttribute("data-type");
+            runGame(gameType);
+            }
+        })
+    }
+    runGame("addition");
 })
 
 
+/** The main gameplay loop, called when the script is first loaded and after the user's answer has been processed */
 
-
-function runGame() {
-document.addEventListener
+function runGame(gameType) {
+    //creates two random integers between 1 and 25
+    let num1 = Math.ceil(Math.random() * 25);
+    let num2 = Math.ceil(Math.random() * 25);
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    } else{
+        alert(`Unknown game type : ${gameType}`)
+        throw `Unknown game type : ${gameType}. Aborting`;
+    }
 }
+
 function checkAnswer() {
 
 }
@@ -32,8 +42,10 @@ function incremementScore() {
 function incremementWrongAnswer() {
 
 }
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+document.getElementById("operand1").textContent = operand1;
+document.getElementById("operand2").textContent = operand2;
+document.getElementById("operator").textContent = "+";
 }
 function displaySubtractQuestion() {
 
