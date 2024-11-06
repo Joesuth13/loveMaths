cdfdf76f6ae45c251d94217b6c70bf46995db716
Samuel Sutherland <joesuth13@gmail.com>
2024-11-06 10:55:29 +0000

    create empty js functions for core game

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame() {
+
+}
+function checkAnswer() {
+
+}
+function calculateCorrectAnswer() {
+
+}
+function incremementScore() {
+
+}
+function incremementWrongAnswer() {
+
+}
+function displayAdditionQuestion() {
+
+}
+function displaySubtractQuestion() {
+
+}
+function displayMultiplyQuestion() {
+
+}
+function displayDivisionQuestion() {
+
+}
